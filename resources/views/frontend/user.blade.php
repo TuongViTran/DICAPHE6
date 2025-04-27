e5587570ec1486ebe2628595ae417575d54e18e2
NhatNguyen-ops <nhatnmpd09600@fpt.edu.vn>
2025-04-27 21:42:38 +0700

    ganlink shopname

--- resources/views/frontend/user.blade.php
+++ resources/views/frontend/user.blade.php
@@ -230,7 +230,14 @@
     <div class="hot-cafes-right" id="cafe-list">
         @foreach($hotCafes as $index => $cafe)
             <div class="cafe-card" data-index="{{ $index }}">
-                <p style="font-size: 22px; font-weight:bold">{{ sprintf('%02d', $index + 1) }} : {{ $cafe->shop_name }}</p >
+            <p style="font-size: 22px; font-weight:bold">
+                {{ sprintf('%02d', $index + 1) }} :
+                @if ($cafe)
+                    <a href="{{ route('frontend.shop', ['id' => $cafe->id]) }}" style="text-decoration: none; color: inherit;">
+                        {{ $cafe->shop_name }}
+                    </a>
+                @endif
+            </p>
                 <div class="cafe-info"> <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="currentColor" class="bi bi-door-open-fill" viewBox="0 0 16 16">
                             <path d="M1.5 15a.5.5 0 0 0 0 1h13a.5.5 0 0 0 0-1H13V2.5A1.5 1.5 0 0 0 11.5 1H11V.5a.5.5 0 0 0-.57-.495l-7 1A.5.5 0 0 0 3 1.5V15zM11 2h.5a.5.5 0 0 1 .5.5V15h-1zm-2.5 8c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1"/>
                             </svg> Giờ mở cửa: {{ $cafe->opening_time }} - {{ $cafe->closing_time }}</div>
